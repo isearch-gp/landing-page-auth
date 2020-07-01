5264c37240e358abef1ca089292653784fc0981f
mkobar <mike@exlorev.com>
2020-07-01 17:22:11 -0400

    added fb values

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "api-key",
-    authDomain: "project-id.firebaseapp.com",
+    apiKey: "AIzaSyA2lPL21fWLnItpIaEc3dHsiQbDiMOfcu0",
+    //authDomain: "project-id.firebaseapp.com",
+    authDomain: "landing-page01.web.app",
     databaseURL: "https://project-id.firebaseio.com",
-    projectId: "project-id",
+    projectId: "landing-page01",
     storageBucket: "project-id.appspot.com",
     messagingSenderId: "sender-id",
-    appId: "app-id",
+    appId: "1:143120279339:web:41e1fa1cb5f471063e876e",
     measurementId: "G-measurement-id"
   }
 };
