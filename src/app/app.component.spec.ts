c9b57c63cac0e60366fa3793e7f9e72c371d07be
mkobar <mike@exlorev.com>
2020-07-02 16:28:27 -0400

    renamed title and removed h1 test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angularfirebase-authentication'`, () => {
+  it(`should have as title 'landing-page-authentication 1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angularfirebase-authentication');
+    expect(app.title).toEqual('landing-page-authentication 1');
   });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angularfirebase-authentication!');
-  });
+  //it('should render title in a h1 tag', () => {
+  //  const fixture = TestBed.createComponent(AppComponent);
+  //  fixture.detectChanges();
+  //  const compiled = fixture.debugElement.nativeElement;
+  //  expect(compiled.querySelector('h1').textContent).toContain('Welcome to landing-page-authentication 1!');
+  //});
 });
