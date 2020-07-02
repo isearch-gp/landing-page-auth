968e24f5c8e8dc0cb04ba6f637ed0004893e2453
mkobar <mike@exlorev.com>
2020-07-02 17:41:55 -0400

    prod settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   firebase: {
+    /***
     apiKey: "api-key",
     authDomain: "project-id.firebaseapp.com",
     databaseURL: "https://project-id.firebaseio.com",
@@ -8,6 +9,16 @@ export const environment = {
     storageBucket: "project-id.appspot.com",
     messagingSenderId: "sender-id",
     appId: "app-id",
+    ***/
+    apiKey: "AIzaSyA2lPL21fWLnItpIaEc3dHsiQbDiMOfcu0",
+    //authDomain: "project-id.firebaseapp.com",
+    authDomain: "landing-page01.web.app",
+    databaseURL: "https://project-id.firebaseio.com",
+    projectId: "landing-page01",
+    storageBucket: "project-id.appspot.com",
+    messagingSenderId: "sender-id",
+    appId: "1:143120279339:web:41e1fa1cb5f471063e876e",
+
     measurementId: "G-measurement-id"
   }
 };
